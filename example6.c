332207517a322da3b3a2e1fb7b5a275c7bf3e066
Your Name <your-email@example.com>
2026-01-07 12:32:22 +0530

    ex6

--- example6.c
+++ example6.c
@@ -2,12 +2,15 @@
 #include <omp.h>
 
 int main() {
+    int counter = 0;
     #pragma omp parallel
     {
         #pragma omp critical
         {
+            counter++;
             printf("Thread %d in critical section. \n", omp_get_thread_num());
         }
     }
+    printf("Final counter value %d\n",counter);
     return 0;
 }
